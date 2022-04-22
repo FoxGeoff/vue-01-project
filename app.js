494829fbf185f6b72e3907a0caa2fa2b0bfca1ce
Geoff Fox <fox.geoffrey@gmail.com>
2022-04-22 15:38:19 -0700

    Add Vue version

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
+// https://vuejs.org/guide/quick-start.html#with-build-tools
+
+Vue.createApp({
+    data() {
+        return {
+            goals: [],
+            enteredValue: ''
+        };
+    },
+    methods: {
+        addGoal() {
+            this.goals.push(this.enteredValue);
+            this.enteredValue= '';
+        }
+    }
+}).mount('#app');
+
+/* js imperitive way
+
 const buttonEl = document.querySelector('button');
 const inputEl = document.querySelector('input');
 const listEl = document.querySelector('ul');
@@ -11,4 +30,4 @@ function addGoal() {
     inputEl.value = '';
 }
 
-buttonEl.addEventListener('click', addGoal);
+buttonEl.addEventListener('click', addGoal); */
